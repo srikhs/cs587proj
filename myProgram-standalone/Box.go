681ee0bfa2a0bad0f0e7e7368fdf27b66005b34a
srikhs <srikhs@gmail.com>
2016-12-08 23:34:25 -0600

    commit on 8th

--- myProgram-standalone/Box.go
+++ myProgram-standalone/Box.go
@@ -9,8 +9,13 @@ import (
 	
 )
 
+func CreateData(x string, y string) MyType{
+	return MyType{x,y}
+}
+
 
 func main () {
+
 	me := syscall.GetUser()
 	path:= "/user/"+me +"/myDir/"
 	fd, statusw := ethos.OpenDirectoryPath(path)
@@ -18,30 +23,37 @@ func main () {
 		log.Fatalf ("Error %v %v", fd, statusw)
 	}	
 	
+	
 	var data MyType
 	status := data.CreateDirectory("/user/said3","")
 	efmt.Println("Sai",status)
 	
-	data.Field1 = "How"
-	data.Field2 = "Are"
+	p1:= CreateData("Great","Human")
+	p2:= CreateData("Humor","Evil")
 
-	efmt.Println("Sai, Input", data)
+	efmt.Println("Sai, Input", p1)
+	efmt.Println("Sai, Input2", p2)
 
-	status = data.WriteVar("/user/said3/foo.txt")
+	status = p1.WriteVar("/user/said3/foo.txt")
+	p2.WriteVar("/user/said3/foo1.txt")
+
 	efmt.Println("Sai, status", status)
 	
 	d:= MyType{"bar","aa"}
 	status = d.ReadVar("/user/said3/foo.txt")
-	
+	e:= MyType{"s","aa"}
+	e.ReadVar("/user/said3/foo1.txt")
 		
 	efmt.Println("Sai" , status)
-	efmt.Println("Sai Read out", d.Field1)
+	efmt.Println("Sai Read out", d)
+	efmt.Println("Sai Read out2",e)
 
 	if d.Field1 == "is"{
 	efmt.Println("Sai found the word")
 	}else
 	{
 	efmt.Println("Sai not found")
-	}
+	}	
 
+	
 	}
